ec466286ac5906709accb22cbcf9f51999d49ea6
Muhammad Zaid <richardmmeans322@gmail.com>
2024-12-06 14:35:46 +0700

    fix: gh pages assets load

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,25 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
+    interface Future {
+        v3_singleFetch: true;
+    }
 }
 
 export default defineConfig({
-  plugins: [
-    remix({
-      ssr: false,
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+    base: "/test-remix-spa/", // Change this to your project's basename
+    plugins: [
+        remix({
+            basename: "/test-remix-spa/", // Change this to your project's basename
+            ssr: false,
+            future: {
+                v3_fetcherPersist: true,
+                v3_relativeSplatPath: true,
+                v3_throwAbortReason: true,
+                v3_singleFetch: true,
+                v3_lazyRouteDiscovery: true,
+            },
+        }),
+        tsconfigPaths(),
+    ],
 });
